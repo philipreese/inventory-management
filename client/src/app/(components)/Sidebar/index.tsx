41362adf125f53369f733cfc8f337d305fd5aa1e
Philip Reese <philipreese@mail.com>
2024-10-04 17:10:58 -0400

    added images

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -12,6 +12,7 @@ import {
   SlidersHorizontal,
   User,
 } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -77,7 +78,15 @@ const Sidebar = () => {
           isSidebarCollapsed ? "px-5" : "px-8"
         }`}
       >
-        <div>logo</div>
+        <div>
+          <Image
+            src="https://s3-inventory-management-philipreese.s3.amazonaws.com/logo.png"
+            alt="philco-logo"
+            width={27}
+            height={27}
+            className="rounded w-8"
+          />
+        </div>
         <h1
           className={`${
             isSidebarCollapsed ? "hidden" : "block"
